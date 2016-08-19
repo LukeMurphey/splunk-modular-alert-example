e6ec0aa54313f09b70490bff7571ef5b7e8db1d9
Luke Murphey <Luke@LukeMurphey.net>
2016-08-19 01:22:24 -0500

    Adding alert action functionality

--- src/bin/make_a_log_message.py
+++ src/bin/make_a_log_message.py
@@ -15,5 +15,18 @@ class MakeLogMessageAlert(ModularAlert):
         
         super(MakeLogMessageAlert, self).__init__(params, logger_name="make_a_log_message_alert", log_level=logging.INFO )
 
+    def make_the_log_message(self, message, importance):
+        """
+        This is the function that does what this modular alert is supposed to do.
+        """
+        self.logger.info("message=%s, importance=%i", message, importance)
+
     def run(self, cleaned_params, payload):
-        pass
+        
+        # Get the information we need to execute the alert action
+        importance = cleaned_params.get('importance', 0)
+        message = cleaned_params.get('message', "(blank)")
+        
+        self.logger.info("Ok, here we go...")
+        self.make_the_log_message(message, importance)
+        self.logger.info("Successfully executed the modular alert. You are a total pro.")
