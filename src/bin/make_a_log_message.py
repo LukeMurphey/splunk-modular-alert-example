226d9e6559d609642f31c38a737314cbe9de2f5b
Luke Murphey <Luke@LukeMurphey.net>
2016-08-19 01:21:27 -0500

    Adding the required functions

--- src/bin/make_a_log_message.py
+++ src/bin/make_a_log_message.py
@@ -1,3 +1,19 @@
 import logging
 import sys
 from modular_alert_example_app.modular_alert import ModularAlert, Field, IntegerField, FieldValidationException
+
+class MakeLogMessageAlert(ModularAlert):
+    """
+    This alert just makes a log message (its an example).
+    """
+    
+    def __init__(self, **kwargs):
+        params = [
+                    IntegerField("importance"),
+                    Field("message")
+        ]
+        
+        super(MakeLogMessageAlert, self).__init__(params, logger_name="make_a_log_message_alert", log_level=logging.INFO )
+
+    def run(self, cleaned_params, payload):
+        pass
